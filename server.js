d20b8a406f478b48479105c9552986144c6d2629
You Song Hou <yousonghou@yahoo.com>
2020-12-07 00:02:27 -0500

    get req.body property by using body-parser

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 const { rewritePdf } = require("./pdf");
 const express = require('express');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res) => res.send('Server Work'));
 
 app.post('/fillform', async (req, res) => {
+    console.log(req.body);
     const pdf = await rewritePdf();
     res.setHeader("Content-Length", pdf.length);
     res.setHeader("Content-Type", "application/pdf");
-    res.setHeader("Content-Disposition", "test.pdf");
+    res.setHeader("Content-Disposition", "filename=test.pdf");
     res.send(new Buffer.from(pdf));
 })
 
